c49f61dc0289f41716826929aa22bbd7fba75349
shilpa <kaboff09@gmail.com>
2023-05-28 17:36:03 +0530

    a new test class added and changed

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.println("Test class created");
+        System.out.println("Test fh class created");
+        System.out.println("fyhryrf");
     }
 }
